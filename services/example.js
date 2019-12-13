295ea6b3e07cd05bf8840433eb9b183263d83388
Pawan Singh <pawanssingh456@gmail.com>
2019-12-14 03:36:49 +0530

    created npm link so that dictionary can be used globally

--- services/example.js
+++ services/example.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const axios = require('axios');
 
-const host = process.env.HOST_API;
-const key = process.env.API_KEY;
+const host = process.env.HOST_API || 'https://fourtytwowords.herokuapp.com';
+const key =
+  process.env.API_KEY ||
+  'b972c7ca44dda72a5b482052b1f5e13470e01477f3fb97c85d5313b3c112627073481104fec2fb1a0cc9d84c2212474c0cbe7d8e59d7b95c7cb32a1133f778abd1857bf934ba06647fda4f59e878d164';
 
 const headers = {
   'Content-Type': 'application/json',
